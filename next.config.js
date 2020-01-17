581271fcbd701fd5a7a040a6a9c4a1057034e326
mhuap <huapmar97@gmail.com>
2020-01-17 01:56:49 -0500

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withSass = require('@zeit/next-sass')
+const debug = process.env.NODE_ENV !== "production";
 module.exports = withSass({
   /* config options here */
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  assetPrefix: isProd ? '/your-repository-name' : '',
 })
