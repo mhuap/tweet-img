6b27bcd17068f94187b5e9f8e92d76e9d49d2610
mhuap <huapmar97@gmail.com>
2020-08-16 05:39:15 -0400

    regen site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "59c7a67b9e9c62b07fc7";
+/******/ 	var hotCurrentHash = "ceaa9c74d6b54e58a6f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
