6c4eaba980ea07c2eef37da79a71bc5c7a121505
mhuap <huapmar97@gmail.com>
2021-05-10 04:48:00 -0400

    twitter picker swatches added back

--- components/colorPicker.js
+++ components/colorPicker.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 
-import { CustomPicker } from 'react-color';
+import { CustomPicker, TwitterPicker } from 'react-color';
 import { EditableInput, Saturation, Hue } from 'react-color/lib/components/common';
 // import { useMediaQuery } from 'react-responsive'
 // import Popover from 'react-bootstrap/Popover';
@@ -11,50 +11,52 @@ import ColorPointer from './colorPointer';
 
 function ColorPicker(props) {
 
-  const colorStyle = {
-    backgroundColor: props.hex,
-  };
-
-  const popover = (
-    <div id='colorpicker-group'>
-
-      <div id='saturation-picker'>
-        <Saturation
-          { ...props }
-          pointer={ ColorPointer }
-          tabIndex='0'
-        />
-      </div>
-
-      <div id='hue-picker'>
-        <Hue
-          {...props}
-          onChange={ props.onChange }
-          direction={ 'horizontal' }
-          pointer={ ColorPointer }
-          tabIndex='0' />
-      </div>
-
-    </div>
-  );
-
   return (
     <div id='colorpicker'>
 
-      <div id="inputgroup" className='soft-outline'>
-        <EditableInput
-          label=""
-          value={ props.hex.substring(1) }
-          onChange={ props.onChange }
-        />
+      <div id='colorpicker-group'>
 
-        <i className="">#</i>
-
-        <div id="swatch" style={colorStyle}>
+        <div id='saturation-picker'>
+          <Saturation
+            { ...props }
+            pointer={ ColorPointer }
+            tabIndex='0'
+          />
         </div>
+
+        <div id='hue-picker'>
+          <Hue
+            {...props}
+            onChange={ props.onChange }
+            direction={ 'horizontal' }
+            pointer={ ColorPointer }
+            tabIndex='0' />
+        </div>
+
+        <div id='swatch-group'>
+
+          <TwitterPicker
+            width='162px'
+            colors={['#FCB900', '#56D150', '#8ED1FC', '#EB144C', '#F7B6DB', '#610DC1']}
+            triangle='hide'
+            onChange={props.onChange}/>
+
+          <div id="inputgroup">
+
+            <EditableInput
+              label=""
+              value={ props.hex.substring(1) }
+              onChange={ props.onChange }
+            />
+
+            <i className="">#</i>
+
+          </div>
+
+        </div>
+
       </div>
 
-      {popover}
 
     </div>
   )
