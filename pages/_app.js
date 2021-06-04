6f85f1296811541a6d88b1f884f31827111eec46
mhuap <huapmar97@gmail.com>
2021-06-04 16:37:11 -0400

    cleaned up scss - all imported in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../scss/index.scss';
+import '../scss/result.scss';
+import '../scss/sidebar.scss';
+import '../scss/tweet.scss';
 
 function TweetImg({ Component, pageProps }) {
   return <Component {...pageProps} />
